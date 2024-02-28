270448a7e2ef05f7cfb55a30a93c1ce1636889fe
Maffiodo <a1122150113@utch.edu.mx>
2024-02-27 22:34:10 -0600

    Loginv1

---
+++ playmusic/src/componentes/NavbarLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavbarLogin() {
+  return (
+    <div>
+    <img src={"./logo.jpeg"} />
+    
+    </div>
+  )
+}
+
+export default NavbarLogin
---
+++ playmusic/src/paginas/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './login.css';
+import NavbarLogin from '../componentes/NavbarLogin';
+
+function Login() {
+    return (
+        <div className="container">
+            <div>
+            <NavbarLogin/>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Login;
