74a624aa1ecdc7e60ad86a39e0046c7aaa03b771
RaquelAlmuina <a1122150127@utch.edu.mx>
2024-02-28 09:41:13 -0600

    V2

--- playmusic/src/componentes/Footer.jsx
+++ playmusic/src/componentes/Footer.jsx
@@ -5,32 +5,32 @@ const Footer = () => {
   return (
     <footer>
         
-    <div class="footer-container">
-        <div class="footer-content-container">
-            <h3 class="website-logo">SoundWave</h3>
-            <span class="footer-info">5573480764</span>
-            <span class="footer-info">SoundWave@Gmail.com</span>
+    <div className="footer-container">
+        <div className="footer-content-container">
+            <h3 className="website-logo">SoundWave</h3>
+            <span className="footer-info">5573480764</span>
+            <span className="footer-info">SoundWave@Gmail.com</span>
         </div>
-        <div class="footer-menus">
-            <div class="footer-content-container">
-                <span class="menu-title">menu</span>
-                <a2 href="" class="menu-item-footer">Contacto</a2>
-                <a2 href="" class="menu-item-footer">Nosotros</a2>
+        <div className="footer-menus">
+            <div className="footer-content-container">
+                <span className="menu-title">menu</span>
+                <a2 href="" className="menu-item-footer">Contacto</a2>
+                <a2 href="" className="menu-item-footer">Nosotros</a2>
             </div>
           
         </div>
         
-        <div class="footer-content-container">
-            <span class="menu-title">Redes Sociales</span>
-            <div class="social-container">
-                <a2 href="" class="social-link"></a2>
-                <a2 href="" class="social-link"></a2>
-                <a2 href="" class="social-link"></a2>
+        <div className="footer-content-container">
+            <span className="menu-title">Redes Sociales</span>
+            <div className="social-container">
+                <a2 href="" className="social-link"></a2>
+                <a2 href="" className="social-link"></a2>
+                <a2 href="" className="social-link"></a2>
             </div>
         </div>
     </div>
-    <div class="copyright-container">
-        <span class="copyright">Copyright 2024, SoundWave. Derechos Reservados.</span>
+    <div className="copyright-container">
+        <span className="copyright">Copyright 2024, SoundWave. Derechos Reservados.</span>
     </div>
 </footer>
 
